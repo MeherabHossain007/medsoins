c6b9425ce898822cb33caa912c24ee3dd3c91fb9
MeherabHossain007 <meherab788@gmail.com>
2026-07-05 00:23:55 +0600

    chore: disable ESLint checks during production builds in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Allow production builds to successfully complete even if
+    // the project has ESLint configuration or rule errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
